c47d26c6a4ec537f73431caea14c32058d64506b
michael-lowe-nz <mikkeyfluttie@gmail.com>
2016-11-06 22:02:05 +1300

    Add form fields html

--- db/getAllTeamsFrom.js
+++ db/getAllTeamsFrom.js
@@ -8,5 +8,5 @@ module.exports = {
 }
 
 function getAllTeamsFrom(league) {
-
+  return knex(league)
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var getAllTeamsFrom = require('../db/getAllTeamsFrom').getAllTeamsFrom
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sports fan synthesizer.' });
+  console.log("At / route")
+  getAllTeamsFrom('superRugbyTeams')
+    .then(function(teams){
+      res.render('index', {teams: teams})
+    })
+    .catch(function(err){
+      console.log(err)
+    })
+  // res.render('index', {title: "Hello"})
 });
 
 module.exports = router;
