9d2eed0a7743d25e9fbc9b0e7068312ef3497c24
michael-lowe-nz <mikkeyfluttie@gmail.com>
2016-11-09 16:38:15 +1300

    Make confusionFixer hacky and working

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var incrementViews = require('../db/incrementViews')
 var getSumOfViews = require('../db/getSumOfViews')
 
 var unirest = require('unirest')
+var confusionFixer = process.NODE_ENV ? 'sum' : 'sum("views")'
+console.log("This is env", process.NODE_ENV)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,7 +23,7 @@ router.get('/showTeam', function(req, res, next){
         team.views ++
         getSumOfViews(req.query.league)
           .then(function(totalViews){
-            team.totalViews = totalViews[0]['sum']
+            team.totalViews = totalViews[0][confusionFixer]
             console.log(team)
             console.log(totalViews)
             res.render('showTeam', team)
