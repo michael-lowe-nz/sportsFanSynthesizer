aaac1c5ec9d08036f34d971a3fbcdfdd43fe9eff
michael-lowe-nz <mikkeyfluttie@gmail.com>
2016-11-24 11:30:20 +1300

    Pre-presensation

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,17 @@ var express = require('express')
 var router = express.Router()
 
 var getAllTeamsFrom = require('../db/getAllTeamsFrom')
+var getSumOfViews = require('../db/getSumOfViews')
 
 router.get('/:league', function (req, res, next) {
-  getAllTeamsFrom('nbaTeams')
+  getAllTeamsFrom(req.params.league)
     .then((teams) => res.json({teams: teams}))
     .catch((error) => res.json(error))
+})
+
+router.get('/:league/sumOfViews', function (req, res, next) {
+  getSumOfViews(req.params.league)
+    .then((sum) => res.json({sum: sum}))
 })
 
 module.exports = router
